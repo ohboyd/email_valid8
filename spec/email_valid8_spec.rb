c02a0ac66a629eee6ac3ee938ec01c59daa2274e
Mike Boyd <mike.boyd@radicalbear.com>
2019-07-16 06:53:20 -0400

    Move code to spec helper to dry it up

--- spec/email_valid8_spec.rb
+++ spec/email_valid8_spec.rb
@@ -1,34 +1,4 @@
-require 'active_model'
-
-class TestEmailModel
-  include ActiveModel::Model
-  attr_accessor :email_by_any_other_name
-
-  def initialize(email_by_any_other_name)
-    @email_by_any_other_name = email_by_any_other_name
-  end
-
-  def []=(attribute, value)
-    public_send("#{attribute}=", value)
-  end
-
-  validates_with EmailAddressValidator, fields: %i[email_by_any_other_name]
-end
-
-class TestEmailArrayModel
-  include ActiveModel::Model
-  attr_accessor :email
-
-  def initialize(email)
-    @email = email
-  end
-
-  def []=(attribute, value)
-    public_send("#{attribute}=", value)
-  end
-
-  validates_with EmailAddressValidator, fields: %i[email], multiples: true
-end
+require 'spec_helper'
 
 RSpec.describe EmailAddressValidator, type: :validator do
   describe 'multiples: false' do
---
+++ spec/spec_helper.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'rspec'
+require 'active_model'
+require 'faker'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'email_valid8'
+
+class TestEmailModel
+  include ActiveModel::Model
+  attr_accessor :email_by_any_other_name
+
+  def initialize(email_by_any_other_name)
+    @email_by_any_other_name = email_by_any_other_name
+  end
+
+  def []=(attribute, value)
+    public_send("#{attribute}=", value)
+  end
+
+  validates_with EmailAddressValidator, fields: %i[email_by_any_other_name]
+end
+
+class TestEmailArrayModel
+  include ActiveModel::Model
+  attr_accessor :email
+
+  def initialize(email)
+    @email = email
+  end
+
+  def []=(attribute, value)
+    public_send("#{attribute}=", value)
+  end
+
+  validates_with EmailAddressValidator, fields: %i[email], multiples: true
+end
